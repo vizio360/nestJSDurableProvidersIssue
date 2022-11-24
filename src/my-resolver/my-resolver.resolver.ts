5556d669a91ea211322b80a9eb99c061b7eb0e9c
Simone Vicentini <simone.vicentini@clubspark.com>
2022-11-24 09:13:30 +0000

    refactor: reintorduced the issue for bug report

--- src/my-resolver/my-resolver.resolver.ts
+++ src/my-resolver/my-resolver.resolver.ts
@@ -1,11 +1,10 @@
-import { Injectable, Scope } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
 import { DurableProviderAService } from '../durable-provider-a/durable-provider-a.service';
 import { NonDurableProviderBService } from '../non-durable-provider-b/non-durable-provider-b.service';
 
 @Resolver()
-//remove the following decorator to see the issue
-@Injectable({ scope: Scope.REQUEST, durable: false })
+//uncomment the following decorator to bypass the issue
+//@Injectable({ scope: Scope.REQUEST, durable: false })
 export class MyResolverResolver {
   constructor(
     private nonDurable: NonDurableProviderBService,
